89c2adc867aaf0f6a5864e8ec919f926960a3833
Jihed Ben Arfa <jihedbenarfa2026@gmail.com>
2026-07-18 13:25:44 +0100

    application entrypoint

---
+++ src/main/java/com/jihedapps/provisioning/ProvisioningApplication.java
@@ -0,0 +1,12 @@
+package com.jihedapps.provisioning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvisioningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProvisioningApplication.class, args);
+    }
+}
